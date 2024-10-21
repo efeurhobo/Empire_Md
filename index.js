208717f9c17dc07a0a1047eb172fbbeef0b03b26
𝐎𝐧𝐥𝐲_𝐨𝐧𝐞_🥇𝐄𝐦𝐩𝐢𝐫𝐞 <162839983+efeurhobo@users.noreply.github.com>
2024-10-22 00:36:31 +0100

    ndex.js

--- index.js
+++ index.js
@@ -1,23 +1,16 @@
+const bot = require(__dirname + '/lib/smd')
+const { VERSION } = require(__dirname + '/config')
+
 const start = async () => {
     Debug.info(`Empire_Md ${VERSION}`)
-    try {
-        await bot.init();
-        bot.logger.info('⏳ Database syncing!');
-        await bot.DATABASE.sync();
-        await bot.connect();
-        
-        // Set up a simple route (for health check or testing)
-        app.get('/', (req, res) => {
-            res.send('Empire_Md is running!');
-        });
-
-        // Start the Express server on the specified PORT
-        app.listen(PORT, () => {
-            console.log(`Server is running on port ${PORT}`);
-        });
-    } catch (error) {
-        Debug.error('Error starting the server:', error);
-        process.exit(1); // Exit the process to let Render restart it
-    }
+  try {
+    await bot.init()
+    bot.logger.info('⏳ Database syncing!')
+    await bot.DATABASE.sync()
+    await bot.connect()
+  } catch (error) {
+    Debug.error(error);
+    start();
+  }
 }
 start();
